fbd9daaa606ef67b6ea221053a302480a99d558a
aeverett11 <aeverett11@usf.edu>
2024-05-31 20:42:50 -0400

    Update cc_5.js
    Implement Employee class with name, salary properties, and annual salary calculation.

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,13 @@
+class Employee {
+    constructor(name, salary) {
+      this.name = name;
+      this.salary = salary;
+      console.log(`Name of the Employee: ${name}`);
+      console.log(`Monthly Salary: $${salary}`);
+    }
+
+    calculateAnnualSalary() {
+        return this.salary * 12;
+      }
+    }
 
