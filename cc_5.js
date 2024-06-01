8e1ea06bb7aae79c788cfd8d544413c83f8a5275
aeverett11 <aeverett11@usf.edu>
2024-05-31 20:43:57 -0400

    Update cc_5.js
    Create Manager subclass with bonus calculation.

--- cc_5.js
+++ cc_5.js
@@ -11,3 +11,15 @@ class Employee {
       }
     }
 
+ class Manager extends Employee {
+      constructor(name, salary, department) {
+        super(name, salary);
+        this.department = department;
+      }
+      calculateAnnualSalary() {
+        const baseSalary = super.calculateAnnualSalary();
+        const bonus = 0.1; 
+        console.log(`Bonus (15% of the base salary): ${bonus}`);
+        // Bonus calculation for managers
+        return (bonus * baseSalary) + baseSalary;
+      }
